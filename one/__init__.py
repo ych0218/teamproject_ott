0d332288bfbd4e670dcdfccfacbad81e0a95a700
ych0218 <gtra464@gmail.com>
2026-04-23 16:46:51 +0900

    20260423ych

--- one/__init__.py
+++ one/__init__.py
@@ -19,9 +19,9 @@ def create_app():
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] =
-    app.config['MAIL_PASSWORD'] =
-    app.config['MAIL_DEFAULT_SENDER'] =
+    app.config['MAIL_USERNAME'] = 'your_email@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'omgjqneijyodwlyh'
+    app.config['MAIL_DEFAULT_SENDER'] = 'your_email@gmail.com'
 
     mail.init_app(app)
 
